0b0f9ca7a9afd905821069c778c94d69161d8d98
AnaRojasP18 <ana.rojas7328@alumnos.udg.mx>
2023-03-21 23:55:43 -0600

    Comiit

---
+++ myapp/src/App.svelte
@@ -0,0 +1,14 @@
+<script>
+    import logo from './lib/images/svelte-welcome.png'
+    import Counter from './routes/Counter.svelte';
+</script>
+
+<main>
+    <img src={logo} alt="Svelte logo" />
+    <h1>Titulo</h1>
+
+    <Counter/>
+    <p>
+        Visit <a href="https://svelte.dev">svelte.dev</a> to jxggxx
+    </p>
+</main>
